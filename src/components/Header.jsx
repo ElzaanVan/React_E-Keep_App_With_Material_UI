804046db2bf173ebdb29603ac2ef38cf9975edd5
ElzaanVan <75164867+ElzaanVan@users.noreply.github.com>
2021-01-09 11:14:38 +0200

    Initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import AddAlertIcon from "@material-ui/icons/AddAlert";
+
+function Header() {
+  return (
+    <header>
+      <h1>
+        <AddAlertIcon />
+        E-Keep
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
